f70013c7463db2fd7a99ad462bc376889209fc56
gdvnprt <gavinkdavenport@gmail.com>
2024-06-12 19:33:19 -0700

    Adjust loop
    now should not skip elements when deleting

--- src/sort.js
+++ src/sort.js
@@ -1,10 +1,11 @@
-export default function sort(array) {
+export default function sortArr(array) {
     //sort array
     let sorted = array.sort(function(a, b){return a - b});
     //remove duplicates
     for(let i = 0; i < sorted.length - 1; i++) {
         if(sorted[i] === sorted[i + 1]) {
             sorted = sorted.splice(i, 1)
+            i--;
         };
     };
     return sorted;
