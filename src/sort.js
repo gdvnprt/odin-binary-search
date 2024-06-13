ce3528efaaf1474be711dfd23ef2e986893a7d2b
gdvnprt <gavinkdavenport@gmail.com>
2024-06-12 19:30:40 -0700

    Simplify sort function
    use base js to simplify and also now removes duplicates

--- src/sort.js
+++ src/sort.js
@@ -1,35 +1,11 @@
-//need to find a way to remove duplicates
-
-export default function mergeSort(array) {
-    //if the array is one number, return that number
-    if (array.length <= 1) {
-        return array;
-    } else {
-        //take the current array and split it in half
-        let halfLength = Math.ceil(array.length / 2);
-
-        //recursively sort each half
-        let leftSide = mergeSort(array.slice(0, halfLength));
-        let rightSide = mergeSort(array.slice(halfLength, array.length));
-
-        //return the sorted array
-        return merge(leftSide, rightSide);
-    };
-};
-
-
-//merge helper
-const merge = (left, right) => {
-    let newArray = [];
-    while (left.length && right.length) {
-        //compare the first place of each array and place the lowest number first
-        if (left[0] <= right[0]) {
-            newArray.push(left[0]);
-            left.shift();
-        } else {
-            newArray.push(right[0]);
-            right.shift();
+export default function sort(array) {
+    //sort array
+    let sorted = array.sort(function(a, b){return a - b});
+    //remove duplicates
+    for(let i = 0; i < sorted.length - 1; i++) {
+        if(sorted[i] === sorted[i + 1]) {
+            sorted = sorted.splice(i, 1)
         };
     };
-return [...newArray, ...left, ...right];
+    return sorted;
 };
