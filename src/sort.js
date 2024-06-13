5489c42318326834601b79e3e32454d9e862cd20
gdvnprt <gavinkdavenport@gmail.com>
2024-06-12 19:58:10 -0700

    Debug
    function now properly removes duplicates

--- src/sort.js
+++ src/sort.js
@@ -2,11 +2,13 @@ export default function sortArr(array) {
     //sort array
     let sorted = array.sort(function(a, b){return a - b});
     //remove duplicates
-    for(let i = 0; i < sorted.length - 1; i++) {
+    let i = 0;
+    while (i < sorted.length - 1) {
         if(sorted[i] === sorted[i + 1]) {
-            sorted = sorted.splice(i, 1)
+            sorted.splice(i, 1);
             i--;
         };
+        i++;
     };
     return sorted;
 };
